46ddbdaab6d40ed756d2477d2be4f115d1321dd3
jasmineanand <jasmineanand2004@gmail.com>
2024-08-22 17:31:32 +0530

    Updated Community Section

--- app.js
+++ app.js
@@ -3,28 +3,40 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-
 // Home page
 app.get('/', function(req, res) {
-res.render('pages/home.ejs');
+    res.render('pages/home.ejs');
 });
 
-
-//About page
+// About page
 app.get('/about', function(req, res) {
-res.render('pages/about.ejs');
+    res.render('pages/about.ejs');
 });
 
-//Community page
+// Community page
 app.get('/community', function(req, res) {
-res.render('pages/community.ejs');
+    res.render('pages/community.ejs');
 });
 
-//Helpline page
+// Helpline page
 app.get('/helpline', function(req, res) {
-res.render('pages/helpline.ejs');
+    res.render('pages/helpline.ejs');
 });
 
+// Donation page
+app.get('/donation', function(req, res) {
+    res.render('pages/donation.ejs');
+});
+
+// Submit Story page
+app.get('/submit-story', function(req, res) {
+    res.render('pages/submit-story.ejs');
+});
+
+// Job Application page
+app.get('/job-application', function(req, res) {
+    res.render('pages/job-application.ejs');
+});
 
 app.listen(3000);
 console.log('Server is listening on port 3000');
